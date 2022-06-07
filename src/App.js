935c472ebecfc763fd50c612b91cc823e10ab614
Daniel Teel <daniel.teel.2@us.af.mil>
2022-06-06 20:23:36 -0600

    added devicePixelRatio scaling to drawing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 
 
 
-const calcNewSize = (editRef, canvasRef, size, lineHeight) => {
+const calcNewSize = (editRef, canvasRef, size, lineHeight, scale) => {
     const newHeight = editRef.current.offsetHeight;
     const ctx = canvasRef.current.getContext('2d', { antialias: false, depth: false });
     ctx.font = String(lineHeight-3) + 'px monospace';
@@ -13,8 +13,8 @@ const calcNewSize = (editRef, canvasRef, size, lineHeight) => {
     const lastLine = firstLine + Math.ceil(editRef.current.offsetHeight / lineHeight);
     let { width: newWidth } = ctx.measureText(String(Math.max(lastLine, 9999)));
     newWidth += 4;
-    if (newHeight !== size.height || newWidth !== size.width) {
-        return { width: newWidth, height: newHeight };
+    if (newHeight !== size.height || newWidth !== size.width || scale!==size.scale) {
+        return { width: newWidth, height: newHeight, scale };
     }
     return null;
 }
@@ -24,7 +24,7 @@ function App({lineHeight=20, lineColorList}) {
     const canvasRef = useRef();
     const [text, setText] = useState('');
     const [scroll, setScroll] = useState({ top: 0, left: 0 });
-    const [size, setSize] = useState({ width: 0, height: 0 })
+    const [size, setSize] = useState({ width: 0, height: 0, pixelRatio: 1 })
     const [repaint, setRepaint] = useState({});
 
     useEffect(() => {
@@ -38,57 +38,67 @@ function App({lineHeight=20, lineColorList}) {
     }, []);
 
     useEffect(() => {
-        const newSize = calcNewSize(editRef, canvasRef, size, lineHeight);
+        const newSize = calcNewSize(editRef, canvasRef, size, lineHeight, window.devicePixelRatio);
         if (newSize) {
             setSize(newSize);
             return;
         }
 
-        const ctx = canvasRef.current.getContext('2d', { antialias: false, depth: false });
+        const drawW=size.width*size.scale;
+        const drawH=size.height*size.scale;
+        const drawLineHeight=lineHeight*size.scale;
+
+        const ctx = canvasRef.current.getContext('2d');
         ctx.fillStyle = '#000000';
-        ctx.fillRect(0, 0, size.width, size.height);
+        ctx.fillRect(0, 0, drawW, drawH);
         ctx.textBaseline = 'middle';
         ctx.textAlign = 'center';
-        ctx.font = String(lineHeight-1) + 'px monospace';
+        ctx.font = String(drawLineHeight) + 'px monospace';
 
         const firstLine = Math.floor(editRef.current.scrollTop / lineHeight);
         const lastLine = firstLine + Math.ceil(editRef.current.offsetHeight / lineHeight);
-        let drawY = (Math.floor(editRef.current.scrollTop / lineHeight) - editRef.current.scrollTop / lineHeight) * lineHeight;
+        let drawY = (Math.floor(editRef.current.scrollTop / lineHeight) - editRef.current.scrollTop / lineHeight) * drawLineHeight+1*size.scale;
 
         for (let line = firstLine; line < lastLine; line++) {
-            ctx.fillStyle = line%2?'#292929':'#111111';
-            ctx.fillRect(0, drawY, size.width, lineHeight);
+            ctx.fillStyle = lineColorList[line+1] ? lineColorList[line+1].bg : line%2?'#1a1a1a':'#141414';
+            ctx.fillRect(0, drawY, drawW, drawLineHeight);
             if (line+1>0){
-                ctx.fillStyle = "#eeeeee";
-                ctx.fillText(String(line + 1), size.width / 2, drawY+lineHeight/2+1);
+                ctx.fillStyle = lineColorList[line+1] ? lineColorList[line+1].fg :"#eeeeee";
+                ctx.fillText(String(line + 1), drawW / 2, drawY+drawLineHeight/2+1);
             }
-            drawY += lineHeight;
+            drawY += drawLineHeight;
         }
 
         ctx.beginPath();
         ctx.strokeStyle='#aaaaaa';
-        ctx.moveTo(size.width-1, 0);
-        ctx.lineTo(size.width-1, size.height);
+        ctx.moveTo(drawW-1, 0);
+        ctx.lineTo(drawW-1, drawH);
         ctx.moveTo(0, 0);
-        ctx.lineTo(0, size.height);
+        ctx.lineTo(0, drawH);
         ctx.stroke();
-    }, [size, scroll, text, repaint, lineHeight])
+    }, [size, scroll, text, repaint, lineHeight, lineColorList])
 
 
     return (
         <>
         <div className='component' style={{ width: '100%', height: '100%' }}>
             <div style={{margin: 0, border: '2px', borderColor:'black'}}>
-            <canvas ref={canvasRef} className='highlighter' width={size.width} height={size.height} style={{ width: size.width, height: size.height }}></canvas>
+            <canvas ref={canvasRef} className='highlighter' width={size.width*size.scale} height={size.height*size.scale} style={{ width: size.width, height: size.height }}></canvas>
             </div>
-            <textarea ref={editRef} className='editor' spellCheck="false" style={{fontSize: (lineHeight-3)+'px', lineHeight: lineHeight+'px'}} defaultValue={`!Yolo Swaggings
-        :FEs
+            <textarea ref={editRef} className='editor' spellCheck="false" style={{fontSize: (lineHeight-3)+'px', lineHeight: lineHeight+'px'}} defaultValue={`
+!Yolo Swaggings
 
-        What is red?
-        a. a color
-        b. a number
-        ans: a
-        `}
+:FEs
+
+    What is red?
+    a. a color
+    b. a number
+    ans: a
+
+    What is blue?
+    *a. a color
+    b. a number 
+`}
                 onInput={(e) => {
                     setText(e.target.value);
                     setScroll({ top: e.target.scrollTop });
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+const lineList={};
+lineList[2]={bg: '#DD5555', fg: '#000000'};
+lineList[4]={bg: '#00daff', fg: '#000000'};
+lineList[6]={bg: '#dd9a00', fg: '#000000'};
+lineList[7]={bg: '#bfca19', fg: '#000000'};
+lineList[8]={bg: '#bfca19', fg: '#000000'};
+lineList[9]={bg: '#3ad200', fg: '#000000'};
+
+lineList[11]={bg: '#dd9a00', fg: '#000000'};
+lineList[12]={bg: '#3ad200', fg: '#000000'};
+lineList[13]={bg: '#bfca19', fg: '#000000'};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App lineColorList={lineList}/>
   </React.StrictMode>
 );
